60061b3e115958ed93e09a816714f3a58497a10e
Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2023-11-03 17:14:13 -0300

    feat: add host build extension for use doppler conventions
    This allow simplify code and use conventions in all our projects

---
+++ Doppler.Extensions/DopplerHostBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Doppler.Extensions.Logging;
+
+using Microsoft.Extensions.Configuration;
+
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+
+namespace Doppler.Extensions
+{
+    public static class DopplerHostBuilderExtensions
+    {
+        public static IHostBuilder UseDopplerConventions(this IHostBuilder builder)
+        {
+            builder
+                .UseSerilog((hostContext, loggerConfiguration) => loggerConfiguration.SetupSeriLog(hostContext.Configuration, hostContext.HostingEnvironment))
+                .ConfigureAppConfiguration((hostContext, config) =>
+                 {
+                     config.AddJsonFile("secret-appsettings.json", true);
+                     config.AddJsonFile($"secret-appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);
+
+                     config.AddJsonFile("/run/secrets/appsettings.json", true);
+                     config.AddJsonFile($"/run/secrets/appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);
+
+                     config.AddJsonFile("/run/secrets/secret-settings.json", true);
+                     config.AddJsonFile("instance-settings.json", true);
+
+                     config.AddJsonFile("appsettings.Secret.json", true);
+                     config.AddJsonFile("/run/secrets/appsettings.Secret.json", true);
+
+                     config.AddEnvironmentVariables();
+                 });
+            return builder;
+        }
+    }
+}
